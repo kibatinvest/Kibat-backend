913d9decf0e1f8766935c7809d630471cf66c708
kibatinvest <bouokelassemon@gmail.com>
2026-08-28 15:56:25 +0000

    Create prisma.module.ts

---
+++ src/prisma/prisma.module.ts
@@ -0,0 +1,11 @@
+// Kibat Invest — module de connexion à la base de données
+import { Global, Module } from '@nestjs/common';
+import { PrismaService } from './prisma.service';
+
+@Global()
+@Module({
+  providers: [PrismaService],
+  exports: [PrismaService],
+})
+export class PrismaModule {}
+  
